f22a5b635417e86ed6341ef47b0b9d2a7d5c065c
ArkadiyUkolov <arkadiy.ukolov@student.manchester.ac.uk>
2018-11-25 04:02:38 +0000

    STORY
    Co-Authored-By: Arina Belova <arinabelova@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ import os
 root=Tk()
 root.title("The GAME")
 
+text_to_show = ('THE GAME')
 
-unlinearity = np.array([2, 3])
-
-text_to_show = ('Wecome traveller, to the World of The Red Communistic Moon!' +
-                '\n\nThis world was created by the will of Six-And-A-Half!' +
-                '\n\nOr so we think.' +
-                '\n\nGood luck.')
 #button1_text = 'A'
 #button2_text = 'B'
 
+event_number = 0
 textbox = 0
 button1 = 0
 button2 = 0
-event_number = 1
-delta = 0
 
-photo = PhotoImage(file='images/1.pgm')
+junctions = np.empty([10, 10])
+junctions[0, 0] = 'story/exam/A'
+junctions[0, 1] = 'story/city'
+
+junctions[1, 0] = 'story/exam/B'
+junctions[1, 1] = 'story/city'
+
+
+way = 'story/exam'
+
+photo = PhotoImage(file=(way + '/1.pgm'))
 
 panelFrame = Frame(root, bg = 'white')
 textFrame = Frame(root, bg = 'black')
@@ -32,102 +36,114 @@ textFrame.pack(side = 'left', fill = 'both', expand = 1)
 
 
 
-def linear():
-    global event_number, unlinearity
-    i = 0
-    while i < np.size(unlinearity):
-        if event_number == unlinearity[i]:
-            return False
-        i += 1
-    return True
+#def linear():
+ #   global event_number, unlinearity
+ #   i = 0
+ #   while i < np.size(unlinearity):
+ #       if event_number == unlinearity[i]:
+ #           return False
+ #       i += 1
+ #   return True
 
 def create_button():
-    global panelFrame, button1, button2
-    
-    button1 = Button(panelFrame, pady = 20)
-    button1.configure(text='A', width = 10, font = 'Arial 30', command=button_clicked1)
-    button1.pack(side = 'left')
+    global panelFrame, button1, button2, event_number
 
-    button2 = Button(panelFrame, pady = 20)
-    button2.configure(text='B', width = 10, font = 'Arial 30')
-
-    print(linear())
-    if linear():
-        button2.configure(command=button_clicked1)
+    if event_number != 3:
+        button1 = Button(panelFrame, pady = 20)
+        button1.configure(text='NEXT', width = 20, font = 'Arial 30')
+        button1.configure(command=button_linear)
+        button1.pack(side = 'bottom')
+       
     else:
-        button2.configure(command=button_clicked2)
+       button1 = Button(panelFrame, pady = 20)
+       button1.configure(text='Option A', width = 10, font = 'Arial 30')
+       button1.configure(command=button_clicked1)
 
-    button2.pack(side = 'right')
+       button2 = Button(panelFrame, pady = 20)
+       button2.configure(text='Option B', width = 10, font = 'Arial 30')
+       button2.configure(command=button_clicked2)
+
+       button1.pack(side = 'left')
+       button2.pack(side = 'right')
+       
 
 def create_text():
     global textFrame, text_to_show, textbox, event_number, photo
     
-    if os.path.isfile('images/{0}.pgm'.format(event_number)) == True:
-        photo = PhotoImage(file = 'images/{0}.pgm'.format(event_number))
-    
+    if os.path.isfile((way + "/{0}.pgm").format(event_number)) == True:
+       photo = PhotoImage(file = (way + "/{0}.pgm").format(event_number))
+
     textbox = Label(textFrame, text = text_to_show)
     textbox.configure(image = photo, anchor = 'w', compound = 'left', justify = 'left',
                       fg = 'white', bg = 'black', font=('Verdand', 20, "bold"), wraplength = 500)
-    textbox.grid(row = 0)
+    
+    textbox.grid(row = 2)
+
+    WAY_TEST = Label(textFrame, text = ('Way: ', (way + "/{0}.pgm").format(event_number)))
+    WAY_TEST.grid(row = 0)
 
 
 
+
+def button_linear():
+    global button1, event_number, textbox
+    
+    textbox.destroy()
+    button1.destroy()
+    
+    event_number += 1
+    
+    event_change()
+    create_button()
+    create_text()
 
 def button_clicked1():
-    global button1, button2, event_number, textbox, delta
-    event_change()
-
+    global button1, button2, event_number, textbox
+    
     textbox.destroy()
     button1.destroy()
     button2.destroy()
-    
+
+    story_way('A')
+    event_change()
     create_button()
     create_text()
-    print(event_number, delta)
-
-    if linear() == False:
-       delta += 1
-       event_number += delta
-    else:
-       event_number += 1
 
 
 def button_clicked2():
-    global button1, button2, event_number, textbox, delta
-    event_change()
+    global button1, button2, event_number, textbox
 
     textbox.destroy()
     button1.destroy()
     button2.destroy()
-    
+
+    story_way('B')
+    event_change()
     create_button()
     create_text()
-    
-    if linear() == False:
-       delta += 2
-       event_number += delta
-    else:
-       event_number += 1
 
-
+def story_way(letter):
+    global way, event_number
+    event_number = 1
+    i = 0
+    size = junctions[0,:]
+    while i < np.size(size):
+        if way == junctions[i, 0]:
+            way = junctions[i, 1]
+        else:
+            way = way + '/' + letter
+    i += 1
 
 def event_change():
-    global event_number, text_to_show, button1_text, button2_text
-    text_to_show = loadtext(event_number)
-#    button1_text = loadbuttons(event_number, 1)
-#    button2_text = loadbuttons(event_number, 2)
+    global text_to_show
+    text_to_show = loadtext()
 
-def loadtext(event):
-    f = open("story/{0}.txt".format(event),"r")
+def loadtext():
+    global way, event_number
+    f = open((way + "/{0}.txt").format(event_number),"r")
     string = f.read()
     return string
 
-#def loadbuttons(event, number):
-#    f = open("button{0}/{1}.txt".format(number, event),"r")
-#    string = f.read()
-#    f.close()
-#    return string
-
 create_button()
-create_text()
+create_text()F
 root.mainloop()
